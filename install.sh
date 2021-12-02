64dd4174196f471d99b28fba35901a459882a2ed
Zexi. Li <lzx12138@sjtu.edu.cn>
2021-12-03 05:05:36 +0800

    feat: create auto sync and install scripts.

--- install.sh
+++ install.sh
@@ -1,9 +1,16 @@
-#! bin/zsh
+#! /bin/zsh
 #
 # TODO: install ranger
 # TODO: install nerd fonts, ranger_devicons
 #
 
+cp -r ./nvim $HOME/.config/nvim
+
 cp -r plug.vim $HOME/.local/share/nvim/site/autoload/
 
-cp init.vim $HOME/.config/nvim/
+cp -r ./ranger $HOME/.config/ranger
+
+cp .tmux.conf $HOME/
+
+cp .zshrc $HOME/
+
--- sync.sh
+++ sync.sh
@@ -1,14 +1,16 @@
 #! /bin/zsh
 
+# nvim config
 cp -r $HOME/.config/nvim ./
 rm -rf ./nvim/tmp
 
+cp $HOME/.local/share/nvim/site/autoload/plug.vim ./
+
+# ranger config
 cp -r $HOME/.config/ranger ./
 rm -rf ./ranger/plugins
 
 cp $HOME/.tmux.conf ./
 
 cp $HOME/.zshrc ./
-
-cp $HOME/.local/share/nvim/site/autoload/plug.vim ./
 
